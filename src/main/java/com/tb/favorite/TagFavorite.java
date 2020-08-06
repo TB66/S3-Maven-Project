186b5e467b4aadede349cb8879ceae223f69014d
TB66 <1132315537@qq.com>
2020-08-06 14:47:45 +0800

    初始化收藏夹功能

---
+++ src/main/java/com/tb/favorite/TagFavorite.java
@@ -0,0 +1,38 @@
+package com.tb.favorite;
+
+/**
+ * java.io.Serializable 序列化接口
+ * 
+ * @author tanben
+ *
+ */
+public class TagFavorite implements java.io.Serializable {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	private Integer tid;
+	private Integer fid;
+
+	public Integer getTid() {
+		return tid;
+	}
+
+	public void setTid(Integer tid) {
+		this.tid = tid;
+	}
+
+	public Integer getFid() {
+		return fid;
+	}
+
+	public void setFid(Integer fid) {
+		this.fid = fid;
+	}
+
+	@Override
+	public String toString() {
+		return "TagFavorite [tid=" + tid + ", fid=" + fid + "]";
+	}
+
+}
